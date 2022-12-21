fc08616331e60a092f3f99122087f96b72a6cc01
Zoyanne <raphaelsicot21@gmail.com>
2022-12-21 21:29:10 +0100

    test

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,17 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Task;
-use App\Form\Type\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 
-
-
 class AccueilController extends AbstractController
 {
-    public function array(): Response
+    public function Restaurants(): Response
     {
         //Création du tableau de restaurants
         $restaurants = array(1 => 'Burger King', 2 => 'Delarte', 3 => 'KFC', 4 => 'Subway');
@@ -24,18 +21,20 @@ class AccueilController extends AbstractController
         ]);
     }
 
-    public function new(Request $request): Response
+    public function addRestaurant(Request $request): Response
     {
         $task = new Task();
-        $task->setTask('Write a blog post');
+
+        $form = $this->createFormBuilder($task)
+            ->add('title')
+            ->getForm();
 
 
-        $form =  $this->createForm(TaskType::class, $task);
         $form->handleRequest($request);
 
 
-        return $this->render('Formulaire.html.twig', [
-            'form' => $form->createView(),
+        return $this->render('Accueil.html.twig', [
+            'formRestaurant' => $form->createView(),
         ]);
     }
 }
