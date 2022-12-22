a5e38d8d069cb2e45fd36b39c3ee424f7309ca24
Zoyanne <raphaelsicot21@gmail.com>
2022-12-22 01:01:35 +0100

    Ajout liste et formulaire restaurant

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,34 +6,34 @@ use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
 class AccueilController extends AbstractController
 {
-    public function Restaurants(): Response
+
+    public function addRestaurant(Request $request): Response
     {
+        //Initialisation 
+        $task = new Task();
+
+        //Création du tableau de restaurants
+        $restaurants = array(1 => 'Burger King', 2 => 'Delarte', 3 => 'KFC', 4 => 'Subway');
+
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class, ['label' => 'Nom du restaurant :'])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer le restaurant'])
+            ->getForm();
+
         //Création du tableau de restaurants
         $restaurants = array(1 => 'Burger King', 2 => 'Delarte', 3 => 'KFC', 4 => 'Subway');
 
         //Renvoi du modèle twig de la page d'accueil
+
         return $this->render('Accueil.html.twig', [
             'restaurants' => $restaurants,
-        ]);
-    }
-
-    public function addRestaurant(Request $request): Response
-    {
-        $task = new Task();
-
-        $form = $this->createFormBuilder($task)
-            ->add('title')
-            ->getForm();
-
-
-        $form->handleRequest($request);
-
-
-        return $this->render('Accueil.html.twig', [
             'formRestaurant' => $form->createView(),
         ]);
     }
