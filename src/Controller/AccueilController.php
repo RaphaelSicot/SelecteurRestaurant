098f44221ffd3d3e3fdd129a48b90158c034cd34
Zoyanne <raphaelsicot21@gmail.com>
2022-12-25 20:26:33 +0100

    récupération des données de la bdd et affichage dans le tableau

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -17,10 +17,14 @@ class AccueilController extends AbstractController
     {
         //Initialisation 
         $task = new Task();
+        $restaurant = '';
 
         //Création du tableau de restaurants
-        $restaurants = array(1 => 'Burger King', 2 => 'Delarte', 3 => 'KFC', 4 => 'Subway');
-        $restaurant =  $restaurants[array_rand($restaurants)];
+        // $restaurants = array(1 => 'Burger King', 2 => 'Delarte', 3 => 'KFC', 4 => 'Subway');
+        $repository = $doctrine->getRepository(Task::class);
+
+        // look for *all* Product objects
+        $restaurants = $repository->findAll();
 
         //Création du formulaire
         $form = $this->createFormBuilder($task)
@@ -39,16 +43,23 @@ class AccueilController extends AbstractController
             if (array_push($restaurants, $form->get('task')->getData())) {
                 $em->persist($task);
                 $em->flush();
-                $this->addFlash('notice', 'Restaurant enregistré!!');
+                $task = new Task();
+                $form = $this->createFormBuilder($task)
+                    ->add('task', TextType::class, ['label' => 'Nom du restaurant :'], ['attr' => ['placeholder' => "Titre du restaurant", 'class' => 'form-control']])
+                    ->add('save', SubmitType::class, ['label' => 'Enregistrer le restaurant'])
+                    ->getForm();
+
+                // return $this -> redirectToRoute('/Accueil)
+
             }
         }
+
 
         //On clique sur le bouton pour pouvoir lancer une sélection aléatoire d'un restaurant
         if (isset($_POST['go'])) {
 
             $restaurant =  $restaurants[array_rand($restaurants)];
         }
-
 
         //Renvoi du modèle twig de la page d'accueil
 
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -2,10 +2,8 @@
 
 namespace App\Entity;
 
-
 use App\Repository\TaskRepository;
 use Doctrine\ORM\Mapping as ORM;
-
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 class Task
@@ -13,26 +11,30 @@ class Task
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private int $id;
-
+    private ?int $id = null;
 
     #[ORM\Column(length: 255)]
     private string $task;
-
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-
-    public function getTask(): string
+    public function __toString()
     {
         return $this->task;
     }
 
-    public function setTask(string $task): void
+    public function getTask(): ?string
+    {
+        return $this->task;
+    }
+
+    public function setTask(string $task): self
     {
         $this->task = $task;
+
+        return $this;
     }
 }
